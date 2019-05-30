2e3ffec286fc2e4441b0354e025dac5500be9dba
David Fellows <dfellows12@gmail.com>
2019-05-30 18:00:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1
+  for i in 2..100
+    if i % num == 0
+      return false
+    end
+  end
+end
+
+# Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
