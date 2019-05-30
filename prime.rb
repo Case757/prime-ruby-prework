fd4c2bf8a9c5204f095645f550c48e41c8144002
David Fellows <dfellows12@gmail.com>
2019-05-30 18:04:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 def prime?(num)
   return false if num <= 1
-  myArr = 2..Math.sqrt(num)
-  for i in myArr
+  myArr = 2..sqrt(num)
+  myArr.each do |i|
+    return false if num % i == 0
     end
   end
 end
